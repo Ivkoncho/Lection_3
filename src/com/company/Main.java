31a1eada6511371ca114b833ddbcfbdebf7a7e92
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-03-01 19:42:03 +0200

    Training Lection 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+        int a=0;
+        int N;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number");
+        N = scan.nextInt();
+        scan.close();
+        while(a<=N) {
+        System.out.println("Next number " + a++);
+
+
+        }
 
 
 
