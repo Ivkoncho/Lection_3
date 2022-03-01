c10240982c3fb97785524d16137689d2c4cfcbd7
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-03-01 20:10:25 +0200

    New Menu-While

---
+++ src/com/company/New.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+public class New {
+    public static void main(String[] args) {
+        int inputNumber=1;
+        Scanner scan = new Scanner(System.in);
+
+
+        while(inputNumber!=0){
+    System.out.println("Please select an option\n" +
+            "1)Option 1\n"+
+            "2)Option 2\n"+
+            "Else -end the program\n");
+            inputNumber = scan.nextInt();
+    switch(inputNumber){
+        case 1:
+            System.out.println("1");
+            break;
+        case 2:
+            System.out.println("2");
+            break;
+        default:
+            inputNumber = 0;
+            System.out.println("Buy");
+            scan.close();
+    }
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+}
